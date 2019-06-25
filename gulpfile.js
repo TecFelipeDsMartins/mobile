4781e82290113160b4bba8eb72dd3b1a93789fb9
Felipe <tec.felipedsmartins@gmail.com>
2019-06-25 15:50:25 -0300

    gulptest

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,5 @@ const gulp = require('gulp');
 const javascriptObfuscator = require('gulp-javascript-obfuscator');
 
 gulp.task('cripto', function(){
-gulp.src('src/*.js')
-    .pipe(javascriptObfuscator())
-    .pipe(gulp.dest('dist'));
+   return console.log("ola");
 });
